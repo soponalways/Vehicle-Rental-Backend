da72ed49ebac59f6ec94e1d7e8d3b54befc74018
Sopon islam <soponalways@gmail.com>
2025-12-30 22:55:22 +0600

    vehicle Database query create

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,9 +10,20 @@ const initDB = async () => {
         email VARCHAR(100) UNIQUE NOT NULL, 
         password TEXT NOT NULL CHECK(LENGTH(password) >= 6), 
         phone VARCHAR(20) NOT NULL, 
-        role VARCHAR(15), 
+        role VARCHAR(15) CHECK (role IN ('admin' , 'customer')), 
         created_at TIMESTAMP DEFAULT NOW(), 
         last_signin_time TIMESTAMP DEFAULT NOW() 
+        )`); 
+    
+    await pool.query(`CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY, 
+        vehicle_name VARCHAR(200) NOT NULL,
+        type VARCHAR(15) CHECK (type IN ('car', 'bike', 'van', 'SUV')), 
+        registration_number VARCHAR(40) UNIQUE NOT NULL, 
+        daily_rent_price INT NOT NULL CHECK (daily_rent_price > 0), 
+        availability_status VARCHAR(20) CHECK ( availability_status IN ('booked' , 'available') ), 
+        created_at TIMESTAMP DEFAULT NOW(), 
+        updated_at TIMESTAMP DEFAULT NOW()
         )`)
 }
 
